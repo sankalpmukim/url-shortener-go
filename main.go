a3c309be3ba35b212057e7d0bc32cd052afc806d
Sankalp Mukim <sankalpmukim@gmail.com>
2023-10-18 18:48:35 +0530

    using go templates

--- main.go
+++ main.go
@@ -1,20 +1,26 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
 
+func handleIndex(w http.ResponseWriter, r *http.Request) {
+	// w.Write([]byte("Hello World!"))
+	tmpl, err := template.ParseFiles("templates/index.html")
+	if err != nil {
+		w.Write([]byte("Error"))
+	}
+	tmpl.Execute(w, "Hello from code")
+}
+
 func main() {
-	fmt.Println("Hello, World!")
 
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello World!"))
-	})
+	r.Get("/", handleIndex)
 	http.ListenAndServe(":3000", r)
 }
